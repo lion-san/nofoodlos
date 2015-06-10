7639f0cad31c050cfce269cc4a83182e7ce2dafe
yokoi <i01210@hotmail.com>
2015-06-10 07:30:41 +0000

    出品

---
+++ bootstrap-3.3.4/js/items.js
@@ -0,0 +1,14 @@
+//スクロールされた時
+var timer = false;
+$(window).scroll(function() {
+    
+    $("#item_regist").fadeOut("normal");
+    
+    if (timer !== false) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(function() {
+        console.log('scroll');
+		$("#item_regist").fadeIn("slow");
+    }, 200);
+});
